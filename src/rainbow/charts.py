391197188c9ddf56cfac7a03f48bb3bbf8e53dd5
Giulia Cantini <giuliacantini3@gmail.com>
2020-01-22 18:03:48 +0100

    Remove smoothing

--- src/rainbow/charts.py
+++ src/rainbow/charts.py
@@ -22,18 +22,18 @@ def main(args):
 	Qs_mean = torch.tensor(Qs, dtype=torch.float32).mean(1).squeeze()
 	
 	fig, (ax1, ax2) = plt.subplots(nrows=2)
-	#plt.plot(episodes, rewards_mean.numpy(), 'r-', linewidth=1.5)
-	ax1.plot(episodes, smooth(rewards_mean.numpy(), 15), 'b-', linewidth=1.5)
+	ax1.plot(episodes, rewards_mean.numpy(), 'b-', linewidth=1.5)
+	#ax1.plot(episodes, smooth(rewards_mean.numpy(), 5), 'b-', linewidth=1.5)
 	ax1.set(ylabel='Reward')
 	ax1.set_title('Learning curve')
-	#plt.plot(episodes, Qs_mean.numpy(), 'r-', linewidth=1.5)
-	ax2.plot(episodes, smooth(Qs_mean.numpy(), 30), 'r-', linewidth=1.5)
+	ax2.plot(episodes, Qs_mean.numpy(), 'r-', linewidth=1.5)
+	#ax2.plot(episodes, smooth(Qs_mean.numpy(), 5), 'r-', linewidth=1.5)
 	ax2.set(xlabel='Episode', ylabel='Q')
 	
 	# Path where to save chart
 	path = args[1].split('/')[:-1]
 	print('/'.join(path))
-	#fig.savefig('/'.join(path)+ "/learning-curve")
+	fig.savefig('/'.join(path)+ "/learning-curve")
 	plt.close(fig)
 
 if __name__ == '__main__':
